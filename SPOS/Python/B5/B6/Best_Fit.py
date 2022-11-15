d8108ea0072e8be5374c6e944515bf7b30fbd44b
Sahil Naik <81952895+Sahil-Naik@users.noreply.github.com>
2022-11-15 20:08:01 +0530

    Create Best_Fit.py

---
+++ SPOS/Python/B5/B6/Best_Fit.py
@@ -0,0 +1,18 @@
+def best_fit(block_size, process_size):
+    memory_required = 99999999
+    blocks_needed = []
+    
+    if process_size <= block_size[-1]:
+        for i in block_size:
+            if i>process_size:
+                temp_mem = i - process_size
+                if memory_required > temp_mem:
+                    memory_required = temp_mem
+                    blocks_needed.append(i)
+                
+        return print("Best fit at block {}.".format(blocks_needed[-1]))
+    
+    else:
+        return print("Process size exceeds available block size!")
+      
+best_fit(block_size, process_size)
